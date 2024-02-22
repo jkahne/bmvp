a7090d2d04c18bf0ed2c6d8e98751305d04c6409
jkahne <jkahne@fastmail.com>
2024-02-21 19:17:59 -0500

    fix preexisting broken test

--- test/bmvp_web/controllers/page_controller_test.exs
+++ test/bmvp_web/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule BmvpWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get(conn, ~p"/")
-    assert html_response(conn, 200) =~ "Peace of mind from prototype to production"
+    assert html_response(conn, 200) =~ "Data to enrich your online business"
   end
 end
